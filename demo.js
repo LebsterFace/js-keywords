6c53515a58055636b2488823cb8e7b050788bb4d
LebsterFace <samsalinsky@gmail.com>
2021-04-13 11:18:37 +0100

    multiple args demo

--- demo.js
+++ demo.js
@@ -1,11 +1,15 @@
-const proxyGen = require("./proxy");
-const proxy = proxyGen({
+const proxy = require("./proxy")({
     print: {
         func: msg => console.log("PRINT: " + msg),
         maxArgs: 1
+    },
+    addAndPrint: {
+        func: (a, b) => console.log(`${a} + ${b} = ${a+b}`),
+        maxArgs: 2
     }
 });
 
 with (proxy) {
 	print, V("Hello world");
+    addAndPrint, V(50), V(25);
 }

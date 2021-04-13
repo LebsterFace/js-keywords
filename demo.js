32dd013d7296e32281bfcd01c29da819fa8500b2
LebsterFace <samsalinsky@gmail.com>
2021-04-13 11:23:48 +0100

    align with readme

--- demo.js
+++ demo.js
@@ -1,15 +1,17 @@
-const proxy = require("./proxy")({
-    print: {
-        func: msg => console.log("PRINT: " + msg),
-        maxArgs: 1
-    },
-    addAndPrint: {
-        func: (a, b) => console.log(`${a} + ${b} = ${a+b}`),
-        maxArgs: 2
-    }
-});
+const useKeywords = require("./proxy");
 
-with (proxy) {
+const keywords = {
+	print: {
+		func: msg => console.log("PRINT: " + msg),
+		maxArgs: 1
+	},
+	addAndPrint: {
+		func: (a, b) => console.log(`${a} + ${b} = ${a + b}`),
+		maxArgs: 2
+	}
+};
+
+with (useKeywords(keywords)) {
 	print, V("Hello world");
-    addAndPrint, V(50), V(25);
+	addAndPrint, V(50), V(25);
 }
